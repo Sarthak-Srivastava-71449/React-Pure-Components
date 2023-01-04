ad69f0b2786ccf6ebe9fa88be67d204c91cb447f
Sarthak-Srivastava-71449 <113346239+Sarthak-Srivastava-71449@users.noreply.github.com>
2023-01-04 15:49:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pure from './Components/Pure';
+import Simple from './Components/Simple'
+
+function App() {
+  return (
+    <div className="App">
+     <Pure />
+     <Simple />
+    </div>
+  );
+}
+
+export default App;
